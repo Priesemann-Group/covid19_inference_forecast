370949e42fd0ef3d482f29e62c063263a30b5206
jdehning <jonas.dehning@ds.mpg.de>
2020-04-04 17:47:00 +0200

    Made some things nicer

--- covid19_inference/plotting.py
+++ covid19_inference/plotting.py
@@ -5,6 +5,11 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 def get_all_free_RVs_names(model):
+    """
+    Returns the names of all free parameters of the model
+    :param model:
+    :return:
+    """
     varnames = [str(x).replace('_log__', '') for x in model.free_RVs]
     return varnames
 
@@ -12,6 +17,16 @@ def get_prior_distribution(model, x, varname):
     return np.exp(model[varname].distribution.logp(x).eval())
 
 def plot_hist(model, trace, ax, varname, colors = ('tab:blue', 'tab:orange'), bins = 50):
+    """
+    Plots one histogram of the prior and posterior distribution of the variable varname.
+    :param model: The model
+    :param trace: the trace
+    :param ax: the axes where to plot
+    :param varname: the variable name to plot
+    :param colors: two color names in which to plot.
+    :param bins: number or array, is passed to np.hist
+    :return: None
+    """
     ax.hist(trace[varname], bins=bins, density=True, color=colors[1],
             label='Posterior')
     limits = ax.get_xlim()
@@ -24,17 +39,18 @@ def plot_hist(model, trace, ax, varname, colors = ('tab:blue', 'tab:orange'), bi
 
 
 def plot_cases(trace, new_cases_obs, date_begin_sim, diff_data_sim, start_date_plot=None, end_date_plot=None,
-               ylim=None):
+               ylim=None, week_interval=None, colors = ('tab:blue', 'tab:orange')):
     """
     Plots the new cases
     :param trace: Needs to have the variables new_cases, delay, μ, λ_t
-    :param new_cases_obs:
-    :param date_begin_sim:
-    :param diff_data_sim:
-    :param start_date_plot:
-    :param end_date_plot:
-    :param ylim:
-    :return:
+    :param new_cases_obs: array, the number of cases each day
+    :param date_begin_sim: datetime, the date of the begin of the simulation
+    :param diff_data_sim: float, the number of days between the begin of the simulation and the begin of the data
+    :param start_date_plot: datetime, the leftmost date that has to be plotted
+    :param end_date_plot: datetime, the rigthmost date to be plotted
+    :param ylim: float, the maximal y value to be plotted
+    :param week_interval: int, the interval in weeks of the y ticks
+    :return: figure, axes
     """
     def conv_time_to_mpl_dates(arr):
         return matplotlib.dates.date2num([datetime.timedelta(days=float(date)) + date_begin_sim for date in arr])
@@ -54,104 +70,94 @@ def plot_cases(trace, new_cases_obs, date_begin_sim, diff_data_sim, start_date_p
     num_days_future = (end_date_plot - date_data_end).days
     start_date_mpl, end_date_mpl = matplotlib.dates.date2num([start_date_plot, end_date_plot])
 
-    color = 'tab:orange'
-    fig, axes = plt.subplots(3, 2, figsize=(8, 8), gridspec_kw={'height_ratios': [1, 3, 3],
+    if week_interval is None:
+        week_inter_left = int(np.ceil(num_days_data/7/5))
+        week_inter_right = int(np.ceil((end_date_mpl - start_date_mpl)/7/6))
+    else:
+        week_inter_left = week_interval
+        week_inter_right = week_interval
+
+    fig, axes = plt.subplots(2, 2, figsize=(9, 5), gridspec_kw={'height_ratios': [1, 3],
                                                                 'width_ratios': [2, 3]})
-    # plt.locator_params(nbins=4)
-    pos_letter = (-0.3, 1)
-    titlesize = 16
 
     ax = axes[1][0]
-    time_arr = np.arange(-len(new_cases_obs) + 1, 1)
+    time_arr = np.arange(-len(new_cases_obs), 0)
     mpl_dates = conv_time_to_mpl_dates(time_arr) + diff_data_sim + num_days_data
-    ax.plot(mpl_dates, new_cases_obs, 'd', markersize=6, label='Data')
+    ax.plot(mpl_dates, new_cases_obs, 'd', markersize=6, label='Data', zorder=5, color=colors[0])
     new_cases_past = new_cases_sim[:, :num_days_data]
     percentiles = np.percentile(new_cases_past, q=2.5, axis=0), np.percentile(new_cases_past, q=97.5, axis=0)
-    ax.plot(mpl_dates, np.median(new_cases_past, axis=0), color=color, label='Fit (with 95% CI)')
-    ax.fill_between(mpl_dates, percentiles[0], percentiles[1], alpha=0.3, color=color)
+    ax.plot(mpl_dates, np.median(new_cases_past, axis=0), color=colors[1], label='Fit (with 95% CI)')
+    ax.fill_between(mpl_dates, percentiles[0], percentiles[1], alpha=0.3, color=colors[1])
     ax.set_yscale('log')
     ax.set_ylabel('Number of new cases')
     ax.set_xlabel('Date')
     ax.legend()
-    ax.xaxis.set_major_locator(matplotlib.dates.WeekdayLocator(interval=1, byweekday=matplotlib.dates.SU))
+    ax.xaxis.set_major_locator(matplotlib.dates.WeekdayLocator(interval = week_inter_left, byweekday=matplotlib.dates.SU))
     ax.xaxis.set_minor_locator(matplotlib.dates.DayLocator())
     ax.xaxis.set_major_formatter(matplotlib.dates.DateFormatter('%m/%d'))
     ax.set_xlim(start_date_mpl)
 
     ax = axes[1][1]
 
-    time1 = np.arange(-len(new_cases_obs) + 1, 1)
+    time1 = np.arange(-len(new_cases_obs) , 0)
     mpl_dates = conv_time_to_mpl_dates(time1) + diff_data_sim + num_days_data
-    ax.plot(mpl_dates, new_cases_obs, 'd', label='Data', markersize=4, color='tab:blue',
+    ax.plot(mpl_dates, new_cases_obs, 'd', label='Data', markersize=4, color=colors[0],
             zorder=5)
 
     new_cases_past = new_cases_sim[:, :num_days_data]
-    ax.plot(mpl_dates, np.median(new_cases_past, axis=0), '--', color=color, linewidth=1.5, label='Fit with 95% CI')
+    ax.plot(mpl_dates, np.median(new_cases_past, axis=0), '--', color=colors[1], linewidth=1.5, label='Fit with 95% CI')
     percentiles = np.percentile(new_cases_past, q=2.5, axis=0), np.percentile(new_cases_past, q=97.5, axis=0)
-    ax.fill_between(mpl_dates, percentiles[0], percentiles[1], alpha=0.2, color=color)
+    ax.fill_between(mpl_dates, percentiles[0], percentiles[1], alpha=0.2, color=colors[1])
 
-    time2 = np.arange(0, num_days_future + 1)
+    time2 = np.arange(0, num_days_future)
     mpl_dates_fut = conv_time_to_mpl_dates(time2) + diff_data_sim + num_days_data
     cases_future = new_cases_sim[:, num_days_data:num_days_data+num_days_future].T
-    # cases_future = np.concatenate([np.ones((1,cases_future.shape[1]))*cases_obs[-1], cases_future], axis=0)
     median = np.median(cases_future, axis=-1)
     percentiles = (
         np.percentile(cases_future, q=2.5, axis=-1),
         np.percentile(cases_future, q=97.5, axis=-1),
     )
-    ax.plot(mpl_dates_fut[1:], median, color=color, linewidth=3, label='forecast with 75% and 95% CI')
-    ax.fill_between(mpl_dates_fut[1:], percentiles[0], percentiles[1], alpha=0.1, color=color)
-    ax.fill_between(mpl_dates_fut[1:], np.percentile(cases_future, q=12.5, axis=-1),
+    ax.plot(mpl_dates_fut, median, color=colors[1], linewidth=3, label='forecast with 75% and 95% CI')
+    ax.fill_between(mpl_dates_fut, percentiles[0], percentiles[1], alpha=0.1, color=colors[1])
+    ax.fill_between(mpl_dates_fut, np.percentile(cases_future, q=12.5, axis=-1),
                     np.percentile(cases_future, q=87.5, axis=-1),
-                    alpha=0.2, color=color)
+                    alpha=0.2, color=colors[1])
 
     ax.set_xlabel('Date')
     ax.set_ylabel('New confirmed cases in Germany')
     ax.legend(loc='upper left')
     ax.set_ylim(0, ylim)
-    # ax.legend(loc='lower left')
-    # ax.set_xticks([-28,-21, -14, -7, 0, 7, 14, 21, 28])
-    # ax.set_xlim(-28, 14)
-    # ax.locator_params(axis="y", nbins=4)
     func_format = lambda num, _: "${:.0f}\,$k".format(num / 1_000)
     ax.yaxis.set_major_formatter(matplotlib.ticker.FuncFormatter(func_format))
     ax.set_xlim(start_date_mpl, end_date_mpl)
-    ax.xaxis.set_major_locator(matplotlib.dates.WeekdayLocator(byweekday=matplotlib.dates.SU))
+    ax.xaxis.set_major_locator(matplotlib.dates.WeekdayLocator(interval = week_inter_right, byweekday=matplotlib.dates.SU))
     ax.xaxis.set_minor_locator(matplotlib.dates.DayLocator())
     ax.xaxis.set_major_formatter(matplotlib.dates.DateFormatter('%m/%d'))
-    ax.text(pos_letter[0], pos_letter[1], "D", transform=ax.transAxes, size=titlesize)
 
     ax = axes[0][1]
 
-    time = np.arange(-diff_to_0 + 1, -diff_to_0 + len_sim + 1)
+    time = np.arange(-diff_to_0 , -diff_to_0 + len_sim )
     λ_t = trace['λ_t'][:, :]
     μ = trace['μ'][:, None]
     mpl_dates = conv_time_to_mpl_dates(time) + diff_data_sim + num_days_data
 
-    ax.plot(mpl_dates, np.median(λ_t - μ, axis=0), color=color, linewidth=2)
+    ax.plot(mpl_dates, np.median(λ_t - μ, axis=0), color=colors[1], linewidth=2)
     ax.fill_between(mpl_dates, np.percentile(λ_t - μ, q=2.5, axis=0), np.percentile(λ_t - μ, q=97.5, axis=0),
                     alpha=0.15,
-                    color=color)
-    # ax.fill_between(mpl_dates, np.percentile(λ_t , q=12.5, axis=0),np.percentile(λ_t, q=87.5, axis=0), alpha=0.2,
-    #                color=color)
+                    color=colors[1])
 
     ax.set_ylabel('effective\ngrowth rate $\lambda_t^*$')
-    # ax.set_xlabel("days from now")
-    # ax.legend(loc='lower left')
+
     ax.set_xticks([-28, -21, -14, -7, 0, 7, 14, 21, 28])
-    # ax.set_xlim(-28, 14)
     ax.set_ylim(-0.15, 0.45)
-    # ax.set_yticks([-0.2, 0, 0.2])
-    # ax.set_aspect(15, adjustable="box")
+
     ax.hlines(0, start_date_mpl, end_date_mpl, linestyles=':')
     delay = matplotlib.dates.date2num(date_data_end) - np.percentile(trace.delay, q=75)
     ax.vlines(delay, -10, 10, linestyles='-', colors=['tab:red'])
-    # ax.legend()
     ax.text(delay + 0.5, 0.4, 'unconstrained because\nof reporting delay', color='tab:red', verticalalignment='top')
     ax.text(delay - 0.5, 0.4, 'constrained\nby data', color='tab:red', horizontalalignment='right',
             verticalalignment='top')
-    ax.text(pos_letter[0], pos_letter[1], "C", transform=ax.transAxes, size=titlesize)
-    ax.xaxis.set_major_locator(matplotlib.dates.WeekdayLocator(byweekday=matplotlib.dates.SU))
+    ax.xaxis.set_major_locator(matplotlib.dates.WeekdayLocator(interval = week_inter_right, byweekday=matplotlib.dates.SU))
     ax.xaxis.set_minor_locator(matplotlib.dates.DayLocator())
     ax.xaxis.set_major_formatter(matplotlib.dates.DateFormatter('%m/%d'))
     ax.set_xlim(start_date_mpl, end_date_mpl)
