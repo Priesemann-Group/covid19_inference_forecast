8dd6d3c97e440330e98f5f7727c560496afa919b
semohr <smohr@malinche.lmp.ds.mpg.de>
2020-06-29 15:07:37 +0200

    automatic figures update

--- scripts/daily_updates/.run_daily_updates.sh
+++ scripts/daily_updates/.run_daily_updates.sh
@@ -2,6 +2,16 @@
 # @Date:   2020-05-29 12:38:21
 # @Last Modified by:   Sebastian
 # @Last Modified time: 2020-05-30 10:28:41
-python weekly_changepoints.py
 
+# Pull from git
+git pull
+# Run the two scripts
+python weekly_changepoints.py
 python what_if_scenarios.py
+# Commit figures
+git add "../../figures/*"
+git commit -m "automatic figures update"
+git push
+
+
+
