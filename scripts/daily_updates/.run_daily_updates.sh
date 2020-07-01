338539a67374dc8053b0c67f07d3684e55742494
semohr <smohr@malinche.lmp.ds.mpg.de>
2020-07-01 15:14:27 +0200

    Update on daily update script

--- scripts/daily_updates/.run_daily_updates.sh
+++ scripts/daily_updates/.run_daily_updates.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # @Author: Sebastian B. Mohr
 # @Date:   2020-05-29 12:38:21
 # @Last Modified by:   Sebastian
@@ -5,12 +6,15 @@
 
 #Change dir to this directory
 this_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd $thisDIR
+
+cd $this_DIR
+
+which python
 # Pull from git
 git pull
 # Run the two scripts
-python weekly_changepoints.py
-python what_if_scenarios.py
+/home/smohr/anaconda3/bin/python weekly_changepoints.py
+/home/smohr/anaconda3/bin/python weekly_changepoints_with_RKI_Nowcast.py
 # Commit figures
 git add "../../figures/*"
 git commit -m "automatic figures update"
