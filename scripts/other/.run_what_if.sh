0b5b822e81cabe433d7cebeac297bd6d7e973cfc
UpdateBot <sebastian.mohr@ds.mpg.de>
2021-01-09 06:14:52 +0100

    automatic figures update

--- scripts/other/.run_what_if.sh
+++ scripts/other/.run_what_if.sh
@@ -4,23 +4,19 @@ this_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $this_DIR
 # >>>  conda initialize >>>
 . /home/smohr/anaconda3/etc/profile.d/conda.sh
-conda activate cc_env
+conda activate base
 # >>>  conda initialize >>>
 
 which python
 # Pull from git
 git pull
 # Run the two scripts in parrallel
-python what_if_scenarios_lockdown_dez.py &> ~/logs/what_if_scenarios_lockdown_dez & P1=$!
-python what_if_scenarios_lockdown_dez_2.py &> ~/logs/what_if_scenarios_lockdown_dez_2 & P2=$!
+#/home/smohr/anaconda3/bin/python3.7 what_if_scenarios_lockdown_dez.py
+/home/smohr/anaconda3/bin/python3.7 what_if_scenarios_lockdown_dez_2.py
 
-# Wait for scripts
-wait $P1
-wait $P2
 
-# Plot new results
-python plot_scenarios_dez.py
-python plot_scenarios_dez_2.py
+/home/smohr/anaconda3/bin/python3.7 plot_scenarios_dez.py
+/home/smohr/anaconda3/bin/python3.7 plot_scenarios_dez_2.py
 
 # Commit figures
 git add "./figures/*"
